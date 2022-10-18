99e17b253b87ec83030f716332643aa38dce0b52
paul <ppierzc@gmail.copm>
2022-10-18 23:09:40 +0200

    add: wehrbein results

--- scripts/external/WehrbeinEtAl/eval_calibration.py
+++ scripts/external/WehrbeinEtAl/eval_calibration.py
@@ -4,16 +4,28 @@ from tqdm import tqdm
 
 import models.model as model
 import config as c
-from utils.eval_functions import (
-    compute_CP_list,
-    pa_hypo_batch,
-    err_3dpe_parallel,
-    compute_3DPCK,
-)
-from utils.data_utils import reinsert_root_joint_torch, root_center_poses
+from utils.eval_functions import (compute_CP_list, pa_hypo_batch,
+                                  err_3dpe_parallel, compute_3DPCK)
+from utils.data_utils import (reinsert_root_joint_torch, root_center_poses)
 import data.data_h36m
 from sklearn.metrics import auc
 from tqdm import tqdm
+import wandb
+import time
+
+wandb.init(
+    project="propose_human36m",
+    entity="ppierzc",
+    job_type="evaluation",
+    name=f"wehrbein_calibration_{time.strftime('%d/%m/%Y::%H:%M:%S')}",
+    config={
+        "seed": 2,
+    },
+)
+
+# Set seed
+torch.manual_seed(wandb.config.seed)
+np.random.seed(wandb.config.seed)
 
 
 print("Program is running on: ", c.device)
@@ -24,7 +36,7 @@ inn.to(c.device)
 inn.load(c.load_model_name, c.device)
 inn.eval()
 
-print(f"{sum(p.numel() for p in inn.parameters()):,}")
+print(f'{sum(p.numel() for p in inn.parameters()):,}')
 
 c.batch_size = 512
 
@@ -37,17 +49,9 @@ cps_step = 1
 cps_length = int((cps_max_th + 1 - cps_min_th) / cps_step)
 
 quick_eval_stride = 16
-test_dataset = data.data_h36m.H36MDataset(
-    c.test_file,
-    quick_eval=True,
-    quick_eval_stride=quick_eval_stride,
-    train_set=False,
-    hardsubset=False,
-)
+test_dataset = data.data_h36m.H36MDataset(c.test_file, quick_eval=True, quick_eval_stride=quick_eval_stride, train_set=False, hardsubset=False)
 
-loader = torch.utils.data.DataLoader(
-    test_dataset, batch_size=1, shuffle=True, drop_last=False
-)
+loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True, drop_last=False)
 
 n_poses = len(test_dataset)
 
@@ -77,12 +81,12 @@ total = 0
 for batch_idx, sample in tqdm(enumerate(loader)):
     # if batch_idx == 10:
     #     break
-    x = sample["poses_3d"]
-    y_gt = sample["p2d_hrnet"]
-    cond = sample["gauss_fits"]
-    occl = sample["occlusions"]
+    x = sample['poses_3d']
+    y_gt = sample['p2d_hrnet']
+    cond = sample['gauss_fits']
+    occl = sample['occlusions']
     bs = x.shape[0]
-    x_gt = sample["p3d_gt"]
+    x_gt = sample['p3d_gt']
 
     # sample multiple z
     z_all = std_dev * torch.randn(n_hypo, bs, c.ndim_z, device=c.device)
@@ -101,8 +105,8 @@ for batch_idx, sample in tqdm(enumerate(loader)):
     x_gt = x_gt.view(1, 3, 17).swapaxes(1, 2)[:, 1:]
 
     sample_median = torch.Tensor(poses_3d_pred).median(0).values
-    errors = ((sample_median - poses_3d_pred) ** 2).sum(-1) ** 0.5
-    true_error = ((sample_median - x_gt) ** 2).sum(-1) ** 0.5
+    errors = ((sample_median - poses_3d_pred) ** 2).sum(-1) ** .5
+    true_error = ((sample_median - x_gt)**2).sum(-1)**.5
 
     q_vals = np.quantile(errors.data.numpy(), quantiles, 0)
     q_val.append(q_vals)
@@ -114,6 +118,10 @@ for batch_idx, sample in tqdm(enumerate(loader)):
 
 quantile_freqs = quantile_counts / total
 
-np.save("wehrbein_quantile_freqs.npy", quantile_freqs)
-np.save("wehrbein_quantiles.npy", quantiles)
-np.save("wehrbein_q_val.npy", q_val)
+calibration_score = np.abs(np.median(quantile_freqs, axis=1) - quantiles).mean()
+wandb.log({"calibration_score": calibration_score})
+print("Calibration score: ", calibration_score)
+
+np.save('wehrbein_quantile_freqs.npy', quantile_freqs)
+np.save('wehrbein_quantiles.npy', quantiles)
+np.save('wehrbein_q_val.npy', q_val)
--- scripts/external/WehrbeinEtAl/eval_calibration_occl.py
+++ scripts/external/WehrbeinEtAl/eval_calibration_occl.py
@@ -4,17 +4,28 @@ from tqdm import tqdm
 
 import models.model as model
 import config as c
-from utils.eval_functions import (
-    compute_CP_list,
-    pa_hypo_batch,
-    err_3dpe_parallel,
-    compute_3DPCK,
-)
-from utils.data_utils import reinsert_root_joint_torch, root_center_poses
+from utils.eval_functions import (compute_CP_list, pa_hypo_batch,
+                                  err_3dpe_parallel, compute_3DPCK)
+from utils.data_utils import (reinsert_root_joint_torch, root_center_poses)
 import data.data_h36m
 from sklearn.metrics import auc
 from tqdm import tqdm
+import wandb
+import time
 
+wandb.init(
+    project="propose_human36m",
+    entity="ppierzc",
+    job_type="evaluation",
+    name=f"wehrbein_calibration_occl_{time.strftime('%d/%m/%Y::%H:%M:%S')}",
+    config={
+        "seed": 2,
+    },
+)
+
+# Set seed
+torch.manual_seed(wandb.config.seed)
+np.random.seed(wandb.config.seed)
 
 print("Program is running on: ", c.device)
 print("EVALUATING EXPERIMENT: ", c.experiment_name, "\n")
@@ -24,7 +35,7 @@ inn.to(c.device)
 inn.load(c.load_model_name, c.device)
 inn.eval()
 
-print(f"{sum(p.numel() for p in inn.parameters()):,}")
+print(f'{sum(p.numel() for p in inn.parameters()):,}')
 
 c.batch_size = 512
 
@@ -37,13 +48,9 @@ cps_step = 1
 cps_length = int((cps_max_th + 1 - cps_min_th) / cps_step)
 
 quick_eval_stride = 16
-test_dataset = data.data_h36m.H36MDataset(
-    c.test_file, quick_eval=False, train_set=False, hardsubset=True
-)
+test_dataset = data.data_h36m.H36MDataset(c.test_file, quick_eval=False, train_set=False, hardsubset=True)
 
-loader = torch.utils.data.DataLoader(
-    test_dataset, batch_size=1, shuffle=True, drop_last=False
-)
+loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True, drop_last=False)
 
 n_poses = len(test_dataset)
 
@@ -74,12 +81,12 @@ e = []
 for batch_idx, sample in tqdm(enumerate(loader)):
     # if batch_idx == 10:
     #     break
-    x = sample["poses_3d"]
-    y_gt = sample["p2d_hrnet"]
-    cond = sample["gauss_fits"]
-    occl = sample["occlusions"]
+    x = sample['poses_3d']
+    y_gt = sample['p2d_hrnet']
+    cond = sample['gauss_fits']
+    occl = sample['occlusions']
     bs = x.shape[0]
-    x_gt = sample["p3d_gt"]
+    x_gt = sample['p3d_gt']
 
     # sample multiple z
     z_all = std_dev * torch.randn(n_hypo, bs, c.ndim_z, device=c.device)
@@ -93,13 +100,9 @@ for batch_idx, sample in tqdm(enumerate(loader)):
 
     poses_3d_pred = reinsert_root_joint_torch(poses_3d_pred)
     poses_3d_pred = root_center_poses(poses_3d_pred) * 1000
-    poses_3d_pred = poses_3d_pred.view(n_hypo, 3, 17).swapaxes(1, 2)[
-        :, np.insert(sample["occlusions"].numpy(), 9, False)
-    ][:, 1:]
+    poses_3d_pred = poses_3d_pred.view(n_hypo, 3, 17).swapaxes(1, 2)[:, np.insert(sample['occlusions'].numpy(), 9, False)][:, 1:]
 
-    x_gt = x_gt.view(1, 3, 17).swapaxes(1, 2)[
-        :, np.insert(sample["occlusions"].numpy(), 9, False)
-    ][:, 1:]
+    x_gt = x_gt.view(1, 3, 17).swapaxes(1, 2)[:, np.insert(sample['occlusions'].numpy(), 9, False)][:, 1:]
     # print(poses_3d_pred.shape)
     #     # poses_3d_pred = poses_3d_pred[:, np.insert(sample['occlusions'].numpy(), 9, False)]
 
@@ -107,27 +110,29 @@ for batch_idx, sample in tqdm(enumerate(loader)):
     e.append(errors_proto1.cpu().numpy())
 
     # print(np.concatenate(e, axis=-1).shape)
-    q = np.quantile(
-        np.concatenate(e, axis=-1), np.array([0, 0.25, 0.5, 0.75, 0.9]), axis=0
-    )
+    # q = np.quantile(np.concatenate(e, axis=-1), np.array([0, 0.25, 0.5, 0.75, 0.9]), axis=0)
     # print(q.shape)
-    print(q.mean(1))
+    # print(q.mean(1))
 
-    # sample_median = torch.Tensor(poses_3d_pred).median(0).values
-    # errors = ((sample_median - poses_3d_pred) ** 2).sum(-1) ** .5
-    # true_error = ((sample_median - x_gt)**2).sum(-1)**.5
+    sample_median = torch.Tensor(poses_3d_pred).median(0).values
+    errors = ((sample_median - poses_3d_pred) ** 2).sum(-1) ** .5
+    true_error = ((sample_median - x_gt)**2).sum(-1)**.5
 
-    # q_vals = np.quantile(errors.data.numpy(), quantiles, 0)
+    q_vals = np.quantile(errors.data.numpy(), quantiles, 0)
     # q_val.append(q_vals)
     #
-    # # v = (q_vals > true_error.data.numpy().squeeze()).astype(int)
-    # v = np.nanmean((q_vals > true_error.data.numpy().squeeze()).astype(int), axis=1)[:, np.newaxis]
-    # if not np.isnan(v).any():
-    #     total += 1
-    #     quantile_counts += v
+    # v = (q_vals > true_error.data.numpy().squeeze()).astype(int)
+    v = np.nanmean((q_vals > true_error.data.numpy().squeeze()).astype(int), axis=1)[:, np.newaxis]
+    if not np.isnan(v).any():
+        total += 1
+        quantile_counts += v
 
-# quantile_freqs = quantile_counts / total
+quantile_freqs = quantile_counts / total
 
-np.save("wehrbein_quantile_freqs_occl.npy", quantile_freqs)
-np.save("wehrbein_quantiles_occl.npy", quantiles)
-np.save("wehrbein_q_val_occl.npy", q_val)
+calibration_score = np.abs(np.median(quantile_freqs, axis=1) - quantiles).mean()
+wandb.log({"calibration_score": calibration_score})
+print("Calibration score: ", calibration_score)
+
+np.save('wehrbein_quantile_freqs_occl.npy', quantile_freqs)
+np.save('wehrbein_quantiles_occl.npy', quantiles)
+np.save('wehrbein_q_val_occl.npy', q_val)
